8fb1a103d4c1528cadd2b3724a190ab50f0fcc5b
Sean Reigle <sereigle@gmail.com>
2017-03-07 13:45:43 -0800

    Made Changes blah blah

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var answer = prompt('');
+
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase () === 'n') {
+  alert('');
+} else {
+  alert('');
+}
